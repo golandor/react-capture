79e79bd2fb9e9f9542030f3675da7d2c26b10f26
Golan Dor <golandor12@gmail.com>
2021-04-21 12:21:14 +0300

    Styled Component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from "react";
 //* import Pages
 import AboutUs from "./pages/AboutUs";
 
+//*import style
+import GlobalStyle from "./components/GlobalStyle";
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
